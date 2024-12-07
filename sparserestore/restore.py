b83d3574e09fcbefea51730e3ef8e933396e8f71
Skadz <46767289+skadz108@users.noreply.github.com>
2024-12-06 22:08:35 -0700

    add crash_on_purpose back

--- sparserestore/restore.py
+++ sparserestore/restore.py
@@ -5,7 +5,8 @@ def restore_file(fp: str, restore_path: str, restore_name: str):
 
     back = backup.Backup(files=[
         backup.Directory("", f"SysContainerDomain-../../../../../../../../var/backup{restore_path}", owner=33, group=33),
-        backup.ConcreteFile("", f"SysContainerDomain-../../../../../../../../var/backup{restore_path}{restore_name}", owner=33, group=33, contents=contents)
+        backup.ConcreteFile("", f"SysContainerDomain-../../../../../../../../var/backup{restore_path}{restore_name}", owner=33, group=33, contents=contents),
+        backup.ConcreteFile("", "SysContainerDomain-../../../../../../../.." + "/crash_on_purpose", contents=b"")
     ])
 
     perform_restore(backup=back)
