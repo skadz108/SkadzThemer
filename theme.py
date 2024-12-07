1ee40a878b00059f59e88a89bf660ff6f03cec9e
Skadz <46767289+skadz108@users.noreply.github.com>
2024-12-06 17:55:41 -0700

    some improvements to the script

--- theme.py
+++ theme.py
@@ -4,7 +4,6 @@ import sys
 from sparserestore import backup, perform_restore
 from sparserestore.restore import restore_file
 from pymobiledevice3.services.installation_proxy import InstallationProxyService
-from pymobiledevice3.services.diagnostics import DiagnosticsService
 from pymobiledevice3.exceptions import NoDeviceConnectedError
 from pymobiledevice3.lockdown import create_using_usbmux
 from pathlib import Path
@@ -46,15 +45,15 @@ def create_catalog_symlinks():
             app_path = Path(value["Path"])
             catalog_path = Path.joinpath(app_path, 'Assets.car').as_posix()
             app_name = Path(catalog_path).parent.name.replace('.app', '')
-#            print(f"app path: {app_path.as_posix()}")
-#            print(f"app name: {app_name}")
-#            print(catalog_path)
             catalog_backup.append(backup.SymbolicLink("", f"SysContainerDomain-../../../../../../../../var/mobile/Library/Logs/RTCReporting/{app_name}-Assets.car.txt", f"{catalog_path}"))
-#    print(catalog_backup)
     return catalog_backup
     
 def apply_catalog():
-    target_path = input("Enter the path to the asset catalog on your iDevice: ")
+    target_path = grab_catalog_path(verbose=False).replace("/private", "")
+    if target_path is None:
+        input("Press Enter to continue...")
+        menu()
+        return
     parts = target_path.split('/')
 
     restore_path = '/'.join(parts[:-1]) + '/'
@@ -65,26 +64,43 @@ def apply_catalog():
     input(f"Press Enter to overwrite {restore_name} with {Path(input_catalog_path).name}...")
 
     restore_file(fp=input_catalog_path, restore_path=restore_path, restore_name=restore_name)
+    input("Restore completed. Press Enter to continue...")
+    menu()
+    return
 
-def grab_catalog_path():
+def grab_catalog_path(verbose: bool = True):
     apps = InstallationProxyService(lockdown).get_apps(calculate_sizes=False, application_type="User")
-    app = input("Enter the bundle name of the app: ")
-    if not app.endswith(".app"):
-        app += ".app"
+    app = input("Enter the name or bundle ID of the app: " if verbose else "Enter the name or bundle ID of the app you'd like to theme: ")
+
     for key, value in apps.items():
-        if isinstance(value, dict) and "Path" in value:
-            app_path = Path(value["Path"])
-            catalog_path = Path.joinpath(app_path, 'Assets.car').as_posix()
-            if app_path.name.lower() == app.lower():
-                print(f"Successfully grabbed path to {app.replace('.app', '')}'s asset catalog!\nCatalog path: {catalog_path}")
-                return
-            # print(f"app path: {app_path.as_posix()}")
-            # print(catalog_path)
+        if isinstance(value, dict):
+            if "CFBundleIdentifier" in value and value["CFBundleIdentifier"] == app:
+                app_path = Path(value["Path"])
+                catalog_path = Path.joinpath(app_path, 'Assets.car').as_posix()
+                if verbose:
+                    print(f"Successfully grabbed path to {app}'s asset catalog!\nCatalog path: {catalog_path}")
+                return catalog_path
+
+            if "Path" in value:
+                app_path = Path(value["Path"])
+                app_name = app_path.name
+                if app_name.lower() == app.lower() or \
+                   app_name.lower() == (app + '.app').lower():
+                    catalog_path = Path.joinpath(app_path, 'Assets.car').as_posix()
+                    if verbose:
+                        print(f"Successfully grabbed path to {app_name.replace('.app', '')}'s asset catalog!\nCatalog path: {catalog_path}")
+                    return catalog_path
+    
+    print(f"No app found with name or bundle ID \"{app}\"")
 
 def menu():
     print(f"""
                   SkadzThemer v1.0
               Brought to you by Skadz
+          
+                 Special thanks to:
+             haxi0    hrtowii    Duy Tran
+              jailbreak.party  NSAntoine
 
       Connected to {lockdown.get_value(key="DeviceName")} ({get_nice_ios_version_string()})
         
@@ -99,7 +115,7 @@ def menu():
     """)
     option = None
     try:
-        user_input = input('Enter a number: ')
+        user_input = input("Select an option: ")
         if user_input.strip():
             option = int(float(user_input))
         else:
